7fdabc28c3f378d67c58b3dbd759eaa3276a23df
Juliane Nagao <julianens@gmail.com>
2020-05-07 15:02:36 +0200

    feat: test Button

---
+++ src/components/Button/Button.test.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import Button from './Button'
+
+describe('Button', () => {
+  let wrapper
+  const mockText = 'Rate'
+  const onClickMock = jest.fn()
+
+  beforeEach(() => {
+    wrapper = shallow(
+      <Button text={mockText} reset={false} onClick={onClickMock} />,
+    )
+  })
+
+  it('should render correctly given the required props', () => {
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('should render one ButtonStyled element of type `button`', () => {
+    expect(wrapper.find('ButtonStyled').length).toEqual(1)
+    expect(wrapper.find('ButtonStyled').prop('type')).toEqual('button')
+  })
+
+  it('should render the button text', () => {
+    expect(wrapper.find('ButtonStyled').children().text()).toEqual('Rate')
+  })
+
+  it('should test click event on button and call onClick one time', () => {
+    wrapper.find('ButtonStyled').simulate('click')
+    expect(onClickMock.mock.calls.length).toEqual(1)
+  })
+})
